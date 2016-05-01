5fc57b802603499224601c739c47f0a4f6024eab
neerajms <neerajms99b@gmail.com>
2016-05-01 11:23:47 +0530

    Added reviews

--- app/src/main/java/com/neerajms99b/neeraj/popularmovies/MovieDetailsFragment.java
+++ app/src/main/java/com/neerajms99b/neeraj/popularmovies/MovieDetailsFragment.java
@@ -1,7 +1,6 @@
 package com.neerajms99b.neeraj.popularmovies;
 
 import android.content.Context;
-import android.media.Image;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,9 +11,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.GridView;
-import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -42,10 +38,14 @@ public class MovieDetailsFragment extends Fragment {
     String mMovieBackDropPath;
 
     ArrayList<String> mTrailersList;
-    private RecyclerView mRecyclerView;
+    ArrayList<ReviewDetails> mReviewsList;
+    private RecyclerView mTrailersRecyclerView;
     private RecyclerView.Adapter mTrailerAdapter;
-    private RecyclerView.LayoutManager mLayoutManager;
+    private RecyclerView.LayoutManager mTrailersLayoutManager;
 
+    private RecyclerView mReviewsRecyclerView;
+    private RecyclerView.Adapter mReviewsAdapter;
+    private RecyclerView.LayoutManager mReviewsLayoutManager;
 
     String mFetchMoviesBaseUrl = "https://api.themoviedb.org/3/movie/";
     final String mApiKeyParam = "api_key";
@@ -115,14 +115,14 @@ public class MovieDetailsFragment extends Fragment {
             @Override
             public void returnResponse(String result) {
 
-                getTrailers(result);
+                getTrailersFromJson(result);
                 Log.d("Response trailers:", result);
             }
         };
         fetchMoviesTask.executeThread(url, queue, volleyCallBack);
     }
 
-    public void getTrailers(String jsonResult) {
+    public void getTrailersFromJson(String jsonResult) {
         String trailerKey;
         final String TMDB_RESULTS = "results";
         final String TMDB_TRAILER_KEY = "key";
@@ -155,16 +155,47 @@ public class MovieDetailsFragment extends Fragment {
         VolleyCallBack volleyCallBack = new VolleyCallBack() {
             @Override
             public void returnResponse(String result) {
-//                try {
-//                    getMoviesDataFromJson(result);
-//                    onPostExecute();
-//                } catch (JSONException je) {
-//
-//                }
+                getReviewsFromJson(result);
                 Log.d("Response reviews:", result);
             }
         };
         fetchMoviesTask.executeThread(url, queue, volleyCallBack);
+    }
+
+    public void getReviewsFromJson(String jsonResult) {
+        String author;
+        String content;
+        final String TMDB_RESULTS = "results";
+        final String TMDB_REVIEW_AUTHOR = "author";
+        final String TMDB_REVIEW_CONTENT = "content";
+        try {
+            JSONObject reviewsJsonObject = new JSONObject(jsonResult);
+            JSONArray reviewsJsonArray = reviewsJsonObject.getJSONArray(TMDB_RESULTS);
+            mReviewsList.clear();
+            for (int i = 0; i < reviewsJsonArray.length(); i++) {
+                author = reviewsJsonArray.getJSONObject(i).getString(TMDB_REVIEW_AUTHOR);
+                content = reviewsJsonArray.getJSONObject(i).getString(TMDB_REVIEW_CONTENT);
+                ReviewDetails reviewDetailsObject = new ReviewDetails(author,content);
+
+                mReviewsList.add(reviewDetailsObject);
+                Log.d("Author:", mReviewsList.get(i).mAuthor);
+                Log.d("Content:", mReviewsList.get(i).mContent);
+
+            }
+            mReviewsAdapter.notifyDataSetChanged();
+
+        } catch (JSONException je) {
+
+        }
+    }
+
+    public class ReviewDetails{
+        String mAuthor;
+        String mContent;
+        public ReviewDetails(String author, String content){
+            this.mAuthor = author;
+            this.mContent = content;
+        }
     }
 
     @Override
@@ -173,16 +204,24 @@ public class MovieDetailsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_movie_details, container, false);
 
         mTrailersList = new ArrayList<String>();
+        mReviewsList = new ArrayList<ReviewDetails>();
         mTrailerAdapter = new TrailerAdapter();
+        mReviewsAdapter = new ReviewsAdapter();
 //        LinearLayout trailerView = (LinearLayout) rootView.findViewById(R.id.trailer_list);
 //        trailerView.
-        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.trailer_recycler_view);
-        mRecyclerView.setHasFixedSize(true);
-        mLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
-        mLayoutManager.canScrollHorizontally();
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mTrailerAdapter);
+        mTrailersRecyclerView = (RecyclerView) rootView.findViewById(R.id.trailer_recycler_view);
+        mTrailersRecyclerView.setHasFixedSize(true);
+        mTrailersLayoutManager = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        mTrailersLayoutManager.canScrollHorizontally();
+        mTrailersRecyclerView.setLayoutManager(mTrailersLayoutManager);
+        mTrailersRecyclerView.setAdapter(mTrailerAdapter);
 
+        mReviewsRecyclerView = (RecyclerView)rootView.findViewById(R.id.reviews_recycler_view);
+        mReviewsRecyclerView.setHasFixedSize(true);
+        mReviewsLayoutManager = new LinearLayoutManager(getActivity(),LinearLayoutManager.VERTICAL,false);
+        mReviewsLayoutManager.canScrollVertically();
+        mReviewsRecyclerView.setLayoutManager(mReviewsLayoutManager);
+        mReviewsRecyclerView.setAdapter(mReviewsAdapter);
 
         TextView movieTitleText = (TextView) rootView.findViewById(R.id.movie_title_text_view);
         movieTitleText.setText(mMovieTitle);
@@ -235,6 +274,54 @@ public class MovieDetailsFragment extends Fragment {
                 v.setLayoutParams(new RecyclerView.LayoutParams(700, 500));
             v.setScaleType(ImageView.ScaleType.FIT_XY);
             ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+
+    }
+    public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ViewHolder> {
+        Context mContext;
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            // each data item is just a string in this case
+            public LinearLayout mReviewsLayout;
+            public TextView mAuthorTextView;
+            public TextView mContentTextView;
+            public ViewHolder(LinearLayout v,TextView authorTextView,TextView contentTextView) {
+                super(v);
+                mReviewsLayout = v;
+                mAuthorTextView = authorTextView;
+                mContentTextView = contentTextView;
+            }
+        }
+
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+//            Picasso.with(getContext()).load(mTrailersList.get(position)).placeholder(R.drawable.placeholder_loading).into(holder.mTrailerImageView);
+            holder.mAuthorTextView.setText(mReviewsList.get(position).mAuthor);
+            holder.mContentTextView.setText(mReviewsList.get(position).mContent);
+        }
+
+        @Override
+        public int getItemCount() {
+            Log.d("Listsize:",String.valueOf(mReviewsList.size()));
+            return mReviewsList.size();
+        }
+
+        @Override
+        public ReviewsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.review_list_item, parent, false);
+            v.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+            TextView author = (TextView)v.findViewById(R.id.author_text_view);
+            TextView content = (TextView)v.findViewById(R.id.content_text_view);
+            ViewHolder vh = new ViewHolder(v,author,content);
             return vh;
         }
 
